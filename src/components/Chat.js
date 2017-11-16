8bcad46c57866bed5a3f16e6e3f4e1327b8c302c
Marcel Widmer <marzelwidmer@gmail.com>
2017-11-16 23:36:12 +0100

    fix chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 
-class Chat extends React.Component {
+class Chat extends Component {
     render() {
         return (
             <View>
-        <Text>Hello Chat</Text>
-      </View>
+                <Text style={styles.text}>Hello {this.props.name} </Text>
+            </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    text: {
+      marginTop: 20,
+      marginLeft: 20,
+      fontSize: 20
+    }
+  });
 export default Chat;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
-class Home extends React.Component {
+class Home extends Component {
+    state ={
+        name : ''
+    }
     render() {
         return (
             <View>
-        <Text>Hello Home</Text>
-      </View>
+                <Text style={styles.title}>
+                    Enter yout name :
+                </Text>
+                <TextInput style={styles.nameInput} 
+                    placeholder='Johne Doe'
+                    onChangeText={(text) =>{
+                        this.setState({
+                            name : text
+                        })
+                    }}
+                    value={this.state.name}
+                />
+                <TouchableOpacity onPress={() =>{
+                        // navigate to second screen and pass it the name
+                        Actions.chat({
+                            name : this.state.name
+                        })
+                    }}
+                >
+                    <Text style={styles.buttonText}>
+                        Next
+                    </Text>
+                </TouchableOpacity>
+            </View>
         );
     }
 }
+const styles = StyleSheet.create({
+  title: {
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 20
+  },
+  nameInput: {
+    padding : 5,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    color : 'black',
+    margin: 20,
+    fontSize: 20
+  },
+  buttonText: {
+    marginLeft: 20,
+    fontSize: 20
+  }
+});
 
 export default Home;
